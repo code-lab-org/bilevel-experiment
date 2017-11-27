1da17ec41636a0cd175bca5c4ebee4e9dafbf94b
Paul T Grogan <paulgrogan@gmail.com>
2017-11-27 00:52:32 -0500

    adds javadoc comments

--- src/main/java/edu/stevens/code/ptg/Round.java
+++ src/main/java/edu/stevens/code/ptg/Round.java
@@ -2,22 +2,44 @@ package edu.stevens.code.ptg;
 
 import java.util.Arrays;
 
+/**
+ * The Class Round.
+ */
 public class Round {
 	private String name;
 	private Task[] tasks;
 	
+	/**
+	 * Instantiates a new round.
+	 */
 	public Round() {
 		this.tasks = new Task[0];
 	}
 	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Gets the tasks.
+	 *
+	 * @return the tasks
+	 */
 	public Task[] getTasks() {
 		return Arrays.copyOf(tasks, tasks.length);
 	}
 	
+	/**
+	 * Gets the task.
+	 *
+	 * @param index the index
+	 * @return the task
+	 */
 	public Task getTask(int index) {
 		if(index < 0 || index >= tasks.length) {
 			throw new IllegalArgumentException("invalid task index");

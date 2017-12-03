e1dc189f416a2cbed94ff3ec9c89637443b75840
Paul T Grogan <paulgrogan@gmail.com>
2017-12-02 19:30:21 -0500

    flash tab if designers are working on different strategies

--- src/main/java/edu/stevens/code/ptg/gui/DesignerUI.java
+++ src/main/java/edu/stevens/code/ptg/gui/DesignerUI.java
@@ -3,12 +3,15 @@ package edu.stevens.code.ptg.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.Timer;
 
 import edu.stevens.code.ptg.Designer;
 import edu.stevens.code.ptg.DesignerApp;
@@ -16,9 +19,11 @@ import edu.stevens.code.ptg.DesignerApp;
 public class DesignerUI extends DesignerAppPanel {
 	private static final long serialVersionUID = 1163389143406697128L;
 
+	private JTabbedPane tabbedPane;
 	private JLabel timeLabel;
 	private DesignUI[] designUIs = new DesignUI[Designer.NUM_STRATEGIES];
 	private StrategyUI strategyUI;
+	private int[] partnerDesigns = new int[Designer.NUM_STRATEGIES];
 	
 	public DesignerUI() {
 		this.setLayout(new BorderLayout());
@@ -27,7 +32,7 @@ public class DesignerUI extends DesignerAppPanel {
 		timeLabel = new JLabel("0", JLabel.RIGHT);
 		timePanel.add(timeLabel);
 		this.add(timePanel, BorderLayout.NORTH);
-		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane = new JTabbedPane();
 		for(int i = 0; i < Designer.NUM_STRATEGIES; i++) {
 			designUIs[i] = new DesignUI(i);
 			tabbedPane.addTab("Design " + i, designUIs[i]);
@@ -43,6 +48,35 @@ public class DesignerUI extends DesignerAppPanel {
 		app.getController().setReadyToShare(true);
 		for(int i = 0; i < Designer.NUM_STRATEGIES; i++) {
 			designUIs[i].bindTo(app);
+		}
+		for(Designer designer : app.getDesigners()) {
+			if(designer != app.getController()) {
+				designer.addObserver(new Observer() {
+					@Override
+					public void update(Observable o, Object arg) {
+						for(int i = 0; i < Designer.NUM_STRATEGIES; i++) {
+							if(designer == app.getDesignPartner() 
+									&& designer.isReadyToShare()) {
+								if(partnerDesigns[i] != designer.getDesign(i)) {
+									partnerDesigns[i] = designer.getDesign(i);
+									if(tabbedPane.getSelectedIndex() != i) {
+										final int strategy = i;
+										tabbedPane.setBackgroundAt(i, Color.decode("#ffcc80"));
+										Timer timer = new Timer(1000, new ActionListener() {
+											@Override
+											public void actionPerformed(ActionEvent e) {
+												tabbedPane.setBackgroundAt(strategy, tabbedPane.getBackground());
+											}
+										});
+										timer.setRepeats(false);
+										timer.start();
+									}
+								}
+							}
+						}
+					}
+				});
+			}
 		}
 		strategyUI.bindTo(app);
 		app.getManager().addObserver(new Observer() {

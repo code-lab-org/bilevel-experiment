64ccb72377418c2400433fa7c23ae8d1b05d36df
Ambrosio Valencia-Romero <avalenci@stevens.edu>
2018-01-29 17:55:39 -0500

    Minor changes to <InstructionsUI.java>.

--- src/main/java/edu/stevens/code/ptg/gui/InstructionUI.java
+++ src/main/java/edu/stevens/code/ptg/gui/InstructionUI.java
@@ -1,5 +1,6 @@
 package edu.stevens.code.ptg.gui;
 
+import java.awt.Font;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -12,6 +13,8 @@ public class InstructionUI extends JEditorPane {
 	private static final long serialVersionUID = -5921608043683745782L;
 	
 	public InstructionUI() {
+		this.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
+		this.setFont(new Font("Helvetica", Font.PLAIN, getFont().getSize()+4));
 		this.setContentType("text/html");
 		this.setEditable(false);
 		this.setFocusable(false);
@@ -21,9 +24,9 @@ public class InstructionUI extends JEditorPane {
 		String text = "<html><center><h1>" + manager.getRoundName() + "</h1>" +
 				"<p>Time Limit: " + Manager.MAX_TASK_TIME + " seconds</p>" +
 				"<h2>Assignments</h2>" +
-				"<table><tr><th>Task</th><th>Designers</th></tr>";
+				"<table><tr><th>Task</th><td><td><td><th>Designers</th></tr>";
 		for(int i = 0; i < Manager.NUM_TASKS; i++) {
-			text += "<tr><td>" + manager.getTask(i).getName() + "</td><td>";
+			text += "<tr><td>" + manager.getTask(i).getName() + "</td><td><td><td><td>";
 			for(int j = 0; j < Task.NUM_DESIGNERS; j++) {
 				text += "Designer " + manager.getTask(i).getDesignerId(j);
 				if(j + 1 < Task.NUM_DESIGNERS) {

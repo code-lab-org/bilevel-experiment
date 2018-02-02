68762fea5f19bd17dbac0cedde7182d54967b471
Paul T. Grogan <pgrogan@stevens.edu>
2018-02-01 20:23:13 -0500

    test code to get fake values working fakely

--- src/main/java/edu/stevens/code/ptg/FakeValueMap.java
+++ src/main/java/edu/stevens/code/ptg/FakeValueMap.java
@@ -2,8 +2,6 @@ package edu.stevens.code.ptg;
 
 public class FakeValueMap {
 	private final ValueMap realValueMap;
-	private double S;
-	private double T;
 	
 	public FakeValueMap(ValueMap realValueMap) {
 		this.realValueMap = realValueMap;
@@ -18,18 +16,38 @@ public class FakeValueMap {
 			int[] values11 = realValueMap.getValues(strategy1, strategy1, design01, design11);
 			// fill in your function here
 			
+			// values00[0]: value for designer 0 under shared strategy 0
+			// values00[1]: value for designer 1 under shared strategy 0
+			// values11[0]: value for designer 0 under shared strategy 1
+			// values11[1]: value for designer 1 under shared strategy 1
+			
 			String SS = realValueMap.getName().substring(0, 2);
+
+			double S = 0;
+			double T = 0;
 			
-			if      (SS == "CH") {S =  0.5; T = 1.5; }
-			else if (SS == "HA") {S =  0.5; T = 0.5; }
-			else if (SS == "PD") {S = -0.5; T = 1.5; }
-			else if (SS == "SH") {S = -0.5; T = 0.5; }
+			if      (SS.equals("CH")) {S =  0.5; T = 1.5; }
+			else if (SS.equals("HA")) {S =  0.5; T = 0.5; }
+			else if (SS.equals("PD")) {S = -0.5; T = 1.5; }
+			else if (SS.equals("SH")) {S = -0.5; T = 0.5; }
 			
-			int v01 = (int) Math.round( S*values00[0] + (1-S)*values11[0] );
-			int v10 = (int) Math.round( T*values00[0] + (1-T)*values11[0] );
+			int[] values = new int[2];
+
+			// value[0]: value for designer 0 under mixed strategy (strategy0, strategy1)
+			// value[1]: value for designer 1 under mixed strategy (strategy0, strategy1)
 			
+			values[0] = (int) Math.round(strategy0==0 ? S*values00[0] + (1-S)*values11[0] : T*values00[0] + (1-T)*values11[0]);
+			values[1] = (int) Math.round(strategy0==0 ? S*values00[1] + (1-S)*values11[1] : T*values00[1] + (1-T)*values11[1]);
 			
-			return new int[]{v01, v10};
+			if(strategy0 == 0) {
+				values[0] = 10;
+				values[1] = 20;
+			} else {
+				values[0] = 30;
+				values[1] = 40;
+			}
+			
+			return values;
 		}
 	}
 }

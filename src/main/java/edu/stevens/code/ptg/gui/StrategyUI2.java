ea6cceb16ad0e0aa71537b3bcf98172fd3daae02
Paul T. Grogan <pgrogan@stevens.edu>
2018-04-13 14:22:48 -0400

    tweak layout of strategy components

--- src/main/java/edu/stevens/code/ptg/gui/StrategyUI2.java
+++ src/main/java/edu/stevens/code/ptg/gui/StrategyUI2.java
@@ -1,12 +1,15 @@
 package edu.stevens.code.ptg.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -23,70 +26,82 @@ import edu.stevens.code.ptg.Manager;
 public class StrategyUI2 extends JPanel {
 	private static final long serialVersionUID = -4318471579781451005L;
 	
+	private JPanel[] strategyPanels = new JPanel[Designer.NUM_STRATEGIES];
 	private JPanel[][] valueContainers = new JPanel[Designer.NUM_STRATEGIES][Designer.NUM_STRATEGIES];
 	private ValueLabel[][] valueLabels = new ValueLabel[Designer.NUM_STRATEGIES][Designer.NUM_STRATEGIES];
 	private JToggleButton[] strategyButtons = new JToggleButton[Designer.NUM_STRATEGIES];
 	
 	public StrategyUI2() {
-		this.setLayout(new GridBagLayout());
+		this.setLayout(new GridLayout(1,0,5,5));
 		
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 0.5;
-		c.weighty = 1;
-		c.insets = new Insets(5,5,5,5);
-		c.fill = GridBagConstraints.BOTH;
 
 		ButtonGroup radios = new ButtonGroup();
 		for(int i = 0; i < Designer.NUM_STRATEGIES; i++) {
-			c.gridy = 0;
-			JLabel agreeLabel = new JLabel("     Agree on " + DesignerUI.STRATEGY_LABELS[i] + "     ", JLabel.CENTER);
-			agreeLabel.setFont(agreeLabel.getFont().deriveFont(24f));
-			agreeLabel.setOpaque(true);
-			agreeLabel.setBackground(DesignerUI.STRATEGY_COLORS[i]);
-			this.add(agreeLabel, c);
-			c.gridy++;
-			valueContainers[i][i] = new JPanel(new BorderLayout());
-			valueContainers[i][i].setBorder(
-					BorderFactory.createEmptyBorder(10,10,10,10));
-			valueContainers[i][i].setBackground(DesignerUI.STRATEGY_COLORS[i]);
-			valueLabels[i][i] = new ValueLabel();
-			valueLabels[i][i].setPreferredSize(new Dimension(100,75));
-			valueContainers[i][i].add(valueLabels[i][i], BorderLayout.CENTER);
-			this.add(valueContainers[i][i], c);
+			strategyPanels[i] = new JPanel();
+			strategyPanels[i].setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+			strategyPanels[i].setLayout(new GridBagLayout());
 			
+			GridBagConstraints c = new GridBagConstraints();
+			c.gridx = 0;
+			c.gridy = 0;
+			c.weightx = 0.5;
+			c.weighty = 1;
+			
+			for(int j : new int[]{i, 1-i}) {
+				c.gridwidth = 1;
+				c.gridy = 0;
+				c.fill = GridBagConstraints.BOTH;
+				
+				JLabel label;
+				if(i == j) {
+					label = new JLabel("     Agree on " + DesignerUI.STRATEGY_LABELS[i] + "     ", JLabel.CENTER);
+					label.setBackground(DesignerUI.STRATEGY_COLORS[i]);
+				} else {
+					label = new JLabel("Disagree (" + DesignerUI.STRATEGY_LABELS[i] + "/" + DesignerUI.STRATEGY_LABELS[1-i] + ")", JLabel.CENTER);
+				}
+				
+				label.setFont(label.getFont().deriveFont(24f));
+				label.setOpaque(true);
+				strategyPanels[i].add(label, c);
+				c.gridy++;
+				valueContainers[i][j] = new JPanel(new BorderLayout());
+				valueContainers[i][j].setBorder(
+						BorderFactory.createEmptyBorder(10,20,10,20));
+				if(i == j) {
+					valueContainers[i][j].setBackground(DesignerUI.STRATEGY_COLORS[i]);
+				}
+				valueLabels[i][j] = new ValueLabel();
+				valueLabels[i][j].setPreferredSize(new Dimension(100,75));
+				valueContainers[i][j].add(valueLabels[i][j], BorderLayout.CENTER);
+				strategyPanels[i].add(valueContainers[i][j], c);
+
+				c.gridx++;
+			}
+
 			c.gridy++;
-			c.gridwidth = 2;
+			c.gridx = 0;
+			c.gridwidth = Designer.NUM_STRATEGIES;
 			c.fill = GridBagConstraints.NONE;
 			strategyButtons[i] = new JToggleButton("Choose " + DesignerUI.STRATEGY_LABELS[i]);
 			strategyButtons[i].setFont(strategyButtons[i].getFont().deriveFont(24f));
 			strategyButtons[i].setHorizontalAlignment(JLabel.CENTER);
 			radios.add(strategyButtons[i]);
-			this.add(strategyButtons[i], c);
+			strategyPanels[i].add(strategyButtons[i], c);
 
-			c.gridx++;
-			c.gridwidth = 1;
-			c.gridy = 0;
-			c.fill = GridBagConstraints.BOTH;
-			JLabel disagreeLabel = new JLabel("Disagree (" + DesignerUI.STRATEGY_LABELS[i] + "/" + DesignerUI.STRATEGY_LABELS[1-i] + ")", JLabel.CENTER);
-			disagreeLabel.setFont(disagreeLabel.getFont().deriveFont(24f));
-			this.add(disagreeLabel, c);
-			c.gridy++;				
-			valueContainers[i][1-i] = new JPanel(new BorderLayout());
-			valueContainers[i][1-i].setBorder(
-					BorderFactory.createEmptyBorder(10,10,10,10));
-			valueLabels[i][1-i] = new FakeValueLabel();
-			valueLabels[i][1-i].setPreferredSize(new Dimension(100,75));
-			valueContainers[i][1-i].add(valueLabels[i][1-i], BorderLayout.CENTER);
-			this.add(valueContainers[i][1-i], c);
-			
-			c.gridx++;
+			final int strategy = i;
+			strategyPanels[i].addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					strategyButtons[strategy].doClick();
+				}
+			});
+			this.add(strategyPanels[i]);
 		}
 	}
 	
 	private void resetUI() {
 		strategyButtons[0].doClick();
+		strategyPanels[0].setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLACK));
 	}
 	
 	@Override
@@ -105,6 +120,15 @@ public class StrategyUI2 extends JPanel {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					app.getController().setStrategy(strategy);
+					for(int i = 0; i < Designer.NUM_STRATEGIES; i++) {
+						if(i == strategy) {
+							strategyPanels[i].setBorder(
+									BorderFactory.createMatteBorder(5, 5, 5, 5, Color.BLACK));
+						} else {
+							strategyPanels[i].setBorder(
+									BorderFactory.createEmptyBorder(5, 5, 5, 5));
+						}
+					}
 				}
 			});
 			for(int j = 0; j < Designer.NUM_STRATEGIES; j++) {
